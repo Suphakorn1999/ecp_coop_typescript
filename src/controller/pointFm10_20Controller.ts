01c700e413778dc54373b1b7ff533da088939aa7
Suphakorn1999 <87161993+Suphakorn1999@users.noreply.github.com>
2022-11-11 10:14:43 +0700

    Update pointFm10_20Controller.ts

--- src/controller/pointFm10_20Controller.ts
+++ src/controller/pointFm10_20Controller.ts
@@ -9,10 +9,8 @@ import { Question } from '../models/questionModel';
 
 export const getFm10_20detail: RequestHandler = async (req, res, next) => {
   const fm20: Array<any> = await Connection.query(
-    `SELECT c.name_company,c.address,c.tel,
+    `SELECT m.idmeeting,c.name_company,c.address,c.tel,
     t.prename_teacher,t.firstname_teacher,t.lastname_teacher,
-    (SELECT CONCAT("[",GROUP_CONCAT(JSON_OBJECT("prename",s.prename_student,"fname",s.fname_student,"lname",s.lname_student,"name_branch",b.name_branch)),"]") 
-    AS student FROM student s GROUP BY sc.idcompany ) AS student,
     CONCAT("[",GROUP_CONCAT(JSON_OBJECT("topic",q.name_question,"point",a.answer)),"]") AS FM10_20 
     FROM student s 
     LEFT JOIN student_company sc ON s.idstudent = sc.idstudent
@@ -27,14 +25,33 @@ export const getFm10_20detail: RequestHandler = async (req, res, next) => {
     LEFT JOIN company c ON sc.idcompany = c.idcompany
     LEFT JOIN factory fa ON b.idfactory = fa.idfactory
     WHERE q.idform = 4
-    GROUP BY f.idfm10_20_coop
+    GROUP BY f.idfm10_20_coop,sc.idcompany
+    `,
+    { type: QueryTypes.SELECT },
+  );
+
+  const fm20_student: Array<any> = await Connection.query(
+    `SELECT m.idmeeting,CONCAT("[",GROUP_CONCAT(JSON_OBJECT("student_id",s.student_id,"prename_student",s.prename_student,"fname_student",s.fname_student,"lname_student",s.lname_student)),"]") AS student
+      FROM student s 
+      LEFT JOIN student_company sc ON s.idstudent = sc.idstudent 
+      LEFT JOIN meeting m ON sc.idstudent_company = m.idstudent_company
+      LEFT JOIN teacher t ON m.idteacher = t.idteacher 
+      LEFT JOIN company c ON sc.idcompany = c.idcompany 
+      LEFT JOIN province p ON c.idprovince = p.idprovince 
+      LEFT JOIN year y ON s.idyear = y.idyear
+      GROUP BY y.idyear
     `,
     { type: QueryTypes.SELECT },
   );
 
   fm20.forEach(async (fm20) => {
-    fm20.student = JSON.parse(fm20.student);
-    fm20.FM10_20 = JSON.parse(fm20.FM10_20);
+    fm20_student.forEach(async (fm20_student) => {
+      if (fm20.idmeeting == fm20_student.idmeeting) {
+        fm20.FM10_20 = JSON.parse(fm20.FM10_20);
+        fm20_student.student = JSON.parse(fm20_student.student);
+        fm20.student = fm20_student.student;
+      }
+    });
   });
 
   return res.status(200).json({
@@ -45,24 +62,20 @@ export const getFm10_20detail: RequestHandler = async (req, res, next) => {
 
 export const getFm10_20coop: RequestHandler = async (req, res, next) => {
   const fm10_20coop: Array<any> = await Connection.query(
-    `SELECT m.idmeeting,c.name_company,c.address,c.tel,
+    `SELECT c.name_company,c.address,c.tel,
     t.prename_teacher,t.firstname_teacher,t.lastname_teacher,
-    (SELECT CONCAT("[",GROUP_CONCAT(JSON_OBJECT("prename",s.prename_student,"fname",s.fname_student,"lname",s.lname_student,"name_branch",b.name_branch)),"]") 
-    AS student FROM student s GROUP BY sc.idcompany ) AS student
+    CONCAT("[",GROUP_CONCAT(JSON_OBJECT("prename",s.prename_student,"fname",s.fname_student,"lname",s.lname_student,"name_branch",b.name_branch)),"]") 
+    AS student
     FROM student s 
     LEFT JOIN student_company sc ON s.idstudent = sc.idstudent
     LEFT JOIN meeting m ON sc.idstudent_company = m.idstudent_company
     LEFT JOIN teacher t ON m.idteacher = t.idteacher
     LEFT JOIN fm10_20_coop f ON m.idmeeting = f.idmeeting
-    LEFT JOIN answerfm10_20 a ON f.idfm10_20_coop = a.idfm10_20_coop
-    LEFT JOIN question q ON a.idquestion = q.idquestion
-    LEFT JOIN form fm ON q.idform = fm.idform
     LEFT JOIN year y ON s.idyear = y.idyear
     LEFT JOIN branch b ON s.idbranch = b.idbranch
     LEFT JOIN company c ON sc.idcompany = c.idcompany
     LEFT JOIN factory fa ON b.idfactory = fa.idfactory
-    WHERE q.idform = 4
-    GROUP BY f.idfm10_20_coop`,
+    GROUP BY c.idcompany`,
     { type: QueryTypes.SELECT },
   );
 
